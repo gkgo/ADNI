80120ad15b17eaa3ffba28e1e30d187efdb791fb
gkgo <91817417+gkgo@users.noreply.github.com>
2023-04-06 15:19:48 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -9,6 +9,7 @@ from tqdm import tqdm  # 添加进度条库
 # from conv import ConvNet4
 from resnet import resnet12
 import matplotlib.pyplot as plt
+import wandb
 wandb.init(project="my-project-name", name="yu")
 
 GPU = torch.cuda.is_available()
